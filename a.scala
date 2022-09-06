c8730626ff00be8ad0910b8cf33a9647dc401284
RusithDhyan <rusithdhyan2000@gmail.com>
2022-09-06 07:14:15 +0530

    answer

--- a.scala
+++ a.scala
@@ -1,16 +1,55 @@
-//import scala.io.StdIn.readInt
-object a extends App{
-    val alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    val E=(c:Char,key:Int,a:String)=>a((a.indexOf(c.toUpper)+key)%a.size)
-    val D=(c:Char,key:Int,a:String)=>a((a.indexOf(c.toUpper)-key)%a.size)
+import scala.io.StdIn.readInt
+import scala.io.StdIn.readLine
 
-    val ct=cipher(E,s,5,alphabet)
-    val pt=cipher(D,ct,5,alphabet)
-    println(ct);
+object Cipher extends App{
 
+    val alphabet = "abcdefghijklmnopqrstuvwxyz"
 
+    val E = (c:Char,key:Int,a:String)=>a((a.indexOf(c.toLower)+key)%a.size)
+    
+    val D = (c:Char,key:Int,a:String)=>a((a.indexOf(c.toLower)-key+a.size)%a.size)
 
+    val cipher = (algo:(Char,Int,String)=>Char,s:String,key:Int,a:String)=>s.map(algo(_,key,a))
 
+    var stop: Boolean = false;
 
+    while(!stop)
+    {
 
-}   
+        print("\n-----------------MENU-----------------\n")
+        print("Enter 'E' to encrypt.\n")
+        print("Enter 'D' to decrypt.\n")
+        print("Enter 'exit' to exit.\n")
+        print("--------------------------------------\n")
+        print("Choose an option: ")
+
+        var decision: String = readLine()
+        
+
+        if(decision == "exit")
+        {
+            stop = true
+        }else if(decision=="D")
+        {
+            print("Enter the string: ")
+            val input: String = readLine()
+            print("Enter the key: ")
+            var key: Int = readInt()
+
+            print("\nDecrypted string is: ")
+            print(cipher(D,input,key,alphabet) + "\n")
+        }else if(decision=="E")
+        {
+            print("Enter the string: ")
+            val input: String = readLine()
+            print("Enter the key: ")
+            var key: Int = readInt()
+
+            print("\nEncrypted string is: ")
+            print(cipher(E,input,key,alphabet) + "\n")
+        }else{
+            print("\nWrong decision input..!\n")
+        }
+        
+    }
+}
